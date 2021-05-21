668606852feb0454dbd8487908e66ed3c77d14c2
Harsh Pareek <32735763+Harsh-Pareek-Commits@users.noreply.github.com>
2021-05-21 14:53:12 +0530

    Add files via upload

---
+++ TripManagementSystem/src/test/java/com/cg/tms/entities/TripManagementSystem/TripManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.tms.entities.TripManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
